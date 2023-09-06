8658ee670092985b5c6a572d9383ee7432a2a51a
yash <yashv@digiflux.io>
2023-09-06 18:04:00 +0530

    change path

--- main.js
+++ main.js
@@ -1,67 +1,78 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
 const renderer = new THREE.WebGL1Renderer({
-  canvas: document.querySelector('#bg')
+  canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(30)
+camera.position.setZ(30);
 
-renderer.render(scene,camera);
-
+renderer.render(scene, camera);
+const glbURL =
+  "https://64f85f827ff7e36e1aae7626--playful-salamander-1d8523.netlify.app/ ";
 
 const loader = new GLTFLoader();
 let headphone;
-loader.load('/assets/headphone/headphone.glb', function ( gltf ) {
-  headphone = gltf.scene;
+
+loader.load(
+  // "/assets/headphone/headphone.glb",
+  "/dist/assets/headphone/headphonemodel.glb",
+  // glbURL,
+  function (gltf) {
+    console.log(gltf, "<<<  glb");
+    headphone = gltf.scene;
     scene.add(headphone);
 
     // Position and scale the model
-    headphone.position.set(0 ,0, 0);
+    headphone.position.set(0, 0, 0);
     headphone.scale.set(25, 25, 25);
 
-
     animate();
+  },
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100) + "% loaded";
+  },
+  function (error) {
+    console.error(error);
+  }
+);
 
-}, function(xhr){
-  console.log(xhr.loaded/xhr.total * 100) + "% loaded";
-}, function ( error ) {
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(1, 1, 3);
 
-	console.error( error );
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper);
 
-} );
-const pointLight = new THREE.PointLight(0xffffff)
-pointLight.position.set(1,1,3)
+const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+const torus = new THREE.Mesh(headphone, material);
 
-const ambientLight = new THREE.AmbientLight(0xffffff)
-scene.add(pointLight,ambientLight )
-const lightHelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200,50)
-scene.add( gridHelper)
+scene.add(torus);
 
-const material = new THREE.MeshStandardMaterial({color: 0xff1fe5ff})
-const torus = new THREE.Mesh(headphone,material)
-
- 
-scene.add(torus)
-
-const controls =  new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
 
 camera.position.z = 5;
 function animate() {
-  requestAnimationFrame(animate)
-  torus.rotation.x += 0.01  
-  torus.rotation.y += 0.01
-  torus.rotation.z += 0.01
-  renderer.render(scene,camera)
+  requestAnimationFrame(animate);
+  // torus.rotation.x += 0.01;
+  // torus.rotation.y += 0.01;
+  // torus.rotation.z += 0.01;
+  renderer.render(scene, camera);
 }
 
-animate()   
+animate();
